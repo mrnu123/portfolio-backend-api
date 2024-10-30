38502d830b74869d51233f4bd7828abea9501702
anuchit <anuchit.nu567@gmail.com>
2024-10-30 22:56:17 +0700

    feat : init folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirestoreService } from './firestore/firestore/firestore.service';
+import { FirestoreService } from './firestore/firestore.service';
+import { CertificatesModule } from './certificates/certificates.module';
+import { BadgesModule } from './badges/badges.module';
 
 @Module({
-  imports: [],
+  imports: [CertificatesModule, BadgesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FirestoreService],
 })
 export class AppModule {}
