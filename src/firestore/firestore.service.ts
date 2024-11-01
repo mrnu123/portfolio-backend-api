ddfc440682ce4c7504c46ec43cb55a51a3e6b242
anuchit <anuchit.nu567@gmail.com>
2024-11-02 00:48:08 +0700

    fix (cred) : change credential from file to env

--- src/firestore/firestore.service.ts
+++ src/firestore/firestore.service.ts
@@ -4,7 +4,7 @@ import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
 import { readFileSync } from 'fs';
 import { join, resolve } from 'path';
-import { environment } from 'src/constant';
+import { firebaseConfig } from 'src/constant';
 
 @Injectable()
 export class FirestoreService {
@@ -12,15 +12,39 @@ export class FirestoreService {
 
   constructor(private configService: ConfigService) {
     try {
-      const serviceAccount: ServiceAccount = JSON.parse(
-        readFileSync(
-          resolve(
-            __dirname,
-            configService.get<string>(environment.FIREBASE_CRED),
-          ),
-          'utf-8',
-        ),
-      );
+      const serviceAccount: ServiceAccount = JSON.parse(`
+        {
+            "type": "${configService.get<string>(firebaseConfig.TYPE)}",
+            "project_id": "${configService.get<string>(
+              firebaseConfig.PROJECT_ID,
+            )}",
+            "private_key_id": "${configService.get<string>(
+              firebaseConfig.PRIVATE_KEY_ID,
+            )}",
+            "private_key": "${configService.get<string>(
+              firebaseConfig.PRIVATE_KEY,
+            )}",
+            "client_email": "${configService.get<string>(
+              firebaseConfig.CLIENT_EMAIL,
+            )}",
+            "client_id": "${configService.get<string>(
+              firebaseConfig.CLIENT_ID,
+            )}",
+            "auth_uri": "${configService.get<string>(firebaseConfig.AUTH_URI)}",
+            "token_uri": "${configService.get<string>(
+              firebaseConfig.TOKEN_URI,
+            )}",
+            "auth_provider_x509_cert_url": "${configService.get<string>(
+              firebaseConfig.AUTH_PROVIDER_X509_CERT_URL,
+            )}",
+            "client_x509_cert_url": "${configService.get<string>(
+              firebaseConfig.CLIENT_X509_CERT_URL,
+            )}",
+            "universe_domain": "${configService.get<string>(
+              firebaseConfig.UNIVERSE_DOMAIN,
+            )}"
+        }
+        `);
 
       if (!admin.apps.length) {
         admin.initializeApp({
